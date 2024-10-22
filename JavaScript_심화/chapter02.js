c94405a62dd26c7482fefbc8ec20e281532e15b4
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-22 17:20:11 +0900

    refactor: 단락평가

--- JavaScript_심화/chapter02.js
+++ JavaScript_심화/chapter02.js
@@ -1,25 +1,31 @@
-// 단가 평가 활용 사례.
+// 단락 평가 활용 사례.
 function returnFalse() {
-    console.log("false함수");
-    return undefined;
+  console.log("false함수");
+  return undefined;
 }
 
 function returnTrue() {
-    console.log("true함수");
-    return 10;
+  console.log("true함수");
+  return 10;
 }
 
 // // console.log(returnFalse() || returnTrue()); // true
 
-// console.log(returnTrue() || returnFalse()); // flase
+// console.log(returnTrue() || returnFalse()); // false
 
-// console.log(returnTrue() && returnFalse()); // flase
+// console.log(returnTrue() && returnFalse()); // false
 
-function printName(person) {
-    const name = person && person.name;
+// const printName = (person) => {
+//   if (!person) {
+//     return "객체가 아닙니다";
+//   }
+//   return person.name;
+// };
 
-    console.log(name || "person의 값이 없음.");
-}
+// 단락평가로 처리하면 훨씬 간결해집니다.
+const printName = (person) => {
+  return person && person.name;
+};
 
 printName();
 printName({ name: "조혜연" });
