ec43c1dd98d72f54c442eafafbdf555226eb3272
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-22 17:06:08 +0900

    refactor: Truthy / falsy

--- JavaScript_심화/chapter1.js
+++ JavaScript_심화/chapter1.js
@@ -1,7 +1,7 @@
 // Truthy와 Falsy
 //참이나 거짓을 의미하지 않는 값도 조건문 내에서 참이나 거짓으로 평가하는 특징.
 
-// falsy한 값.
+// falsy한 값. 조건문에서 거짓으로 평가.
 let f1 = undefined;
 let f2 = null;
 let f3 = 0;
@@ -19,11 +19,16 @@ let t5 = () => {};
 
 // 활용 사례
 function printName(person) {
-    if (!person) {
-        console.log("person값이 없음");
-        return;
-    }
-    console.log(person.name);
+  // null이거나 undefined가 아닌지 먼저 확인 필요 , 비효율적인 방식
+  //   if (person === undefined || person === null) {
+  //   }
+
+  // Truthy / falsy
+  if (!person) {
+    console.log("person값이 없음");
+    return;
+  }
+  console.log(person.name);
 }
 
 let person = { name: "조혜연" };
